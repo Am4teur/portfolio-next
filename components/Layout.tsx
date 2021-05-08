54cbeb1ff66f1a5301af2d08f8cb51edf22d8042
am4teur <daniel.7c.n12@gmail.com>
2021-05-08 15:16:49 +0100

    Layout with Sidebar

---
+++ components/Layout.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+
+//import styles from '../styles/Layout.module.scss';
+import styles from '../styles/Home.module.scss';
+
+interface layoutProps {
+  children: JSX.Element,
+}
+
+export default function Layout({ children }: layoutProps) {
+  return (
+    <div>
+      <Sidebar/>
+      
+      <div className={styles.container}>
+        {children}
+        <Footer/>
+      </div>
+
+    </div>
+  )
+};
---
+++ components/Sidebar.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import styles from '../styles/Home.module.scss';
+
+export default function Sidebar() {
+  return (
+    <nav className={styles.sidebar}>
+      <ul className={styles.sidebar_items}>
+        <li className={styles.sidebar_logo}>
+            <Link href="/">
+              <a className={styles.sidebar_link}>
+                <span className={styles.item_text}>DC</span>
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right" className="svg-inline--fa fa-arrow-right fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path></svg>
+              </a>
+            </Link>
+        </li>
+        <li className={styles.sidebar_item}>
+            <Link href="/">
+              <a className={styles.sidebar_link}>
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home" className="svg-inline--fa fa-home fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg>
+                <span className={styles.item_text}>Home</span>
+              </a>
+            </Link>
+        </li>
+        <li className={styles.sidebar_item}>
+            <Link href="/">
+              <a className={styles.sidebar_link}>
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" className="svg-inline--fa fa-user fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path></svg>
+                <span className={styles.item_text}>About</span>
+              </a>
+            </Link>
+        </li>
+        <li className={styles.sidebar_item}>
+            <Link href="/">
+              <a className={styles.sidebar_link}>
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="project-diagram" className="svg-inline--fa fa-project-diagram fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M384 320H256c-17.67 0-32 14.33-32 32v128c0 17.67 14.33 32 32 32h128c17.67 0 32-14.33 32-32V352c0-17.67-14.33-32-32-32zM192 32c0-17.67-14.33-32-32-32H32C14.33 0 0 14.33 0 32v128c0 17.67 14.33 32 32 32h95.72l73.16 128.04C211.98 300.98 232.4 288 256 288h.28L192 175.51V128h224V64H192V32zM608 0H480c-17.67 0-32 14.33-32 32v128c0 17.67 14.33 32 32 32h128c17.67 0 32-14.33 32-32V32c0-17.67-14.33-32-32-32z"></path></svg>
+                <span className={styles.item_text}>Projects</span>
+              </a>
+            </Link>
+        </li>
+        <li className={styles.sidebar_item}>
+            <Link href="/contact">
+              <a className={styles.sidebar_link}>
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="blog" className="svg-inline--fa fa-blog fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M172.2 226.8c-14.6-2.9-28.2 8.9-28.2 23.8V301c0 10.2 7.1 18.4 16.7 22 18.2 6.8 31.3 24.4 31.3 45 0 26.5-21.5 48-48 48s-48-21.5-48-48V120c0-13.3-10.7-24-24-24H24c-13.3 0-24 10.7-24 24v248c0 89.5 82.1 160.2 175 140.7 54.4-11.4 98.3-55.4 109.7-109.7 17.4-82.9-37-157.2-112.5-172.2zM209 0c-9.2-.5-17 6.8-17 16v31.6c0 8.5 6.6 15.5 15 15.9 129.4 7 233.4 112 240.9 241.5.5 8.4 7.5 15 15.9 15h32.1c9.2 0 16.5-7.8 16-17C503.4 139.8 372.2 8.6 209 0zm.3 96c-9.3-.7-17.3 6.7-17.3 16.1v32.1c0 8.4 6.5 15.3 14.8 15.9 76.8 6.3 138 68.2 144.9 145.2.8 8.3 7.6 14.7 15.9 14.7h32.2c9.3 0 16.8-8 16.1-17.3-8.4-110.1-96.5-198.2-206.6-206.7z"></path></svg>
+                <span className={styles.item_text}>Blog</span>
+              </a>  
+            </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
