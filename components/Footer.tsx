7d47fe71f28353f4c51b4f8edd3cc718d76cf410
am4teur <daniel.7c.n12@gmail.com>
2021-05-08 15:16:32 +0100

    Initial footer

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/Home.module.scss';
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+      footer
+    </div>
+  )
+}
